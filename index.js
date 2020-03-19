37c0544864a7958f79fe75db35303b808db87950
Alejandro Masin <masinalejandro.crosetto@gmail.com>
2020-03-19 15:45:38 -0300

    correccion

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ function saludo(name = "Alejandro") {
     return `Hola, ${name}`;
   }
   
-console.log('------------------------------------');
-console.log(saludo());
-console.log('------------------------------------');
 
 module.exports = {
-  saludo
+  saludo,
 };
